fd451a75e4bda75b51a1355ee95ead3440231d6e
AgbVal <52071388+AgbVal@users.noreply.github.com>
2022-08-27 06:50:03 +0100

    Finished Second Assignment

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float horizontalInput;
+    private float speed = 10.0f;
 
     // Update is called once per frame
     void Update()
     {
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.left * horizontalInput * Time.deltaTime * speed);
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -12,21 +12,28 @@ public class SpawnManagerX : MonoBehaviour
 
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
+    private float spawnTime;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        StartCoroutine(SpawnRandomBall());
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    IEnumerator SpawnRandomBall()
     {
+        spawnTime = Random.Range(2, 5);
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
-
+        // wait for random seconds before spawning the ball
+        yield return new WaitForSeconds(spawnTime);
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        // call back
+        yield return StartCoroutine(SpawnRandomBall());
     }
 
 }
